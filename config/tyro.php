e77d66c7fb14746dad405f81cb53d555a25dba89
Hasin Hayder <countdraculla@gmail.com>
2026-01-21 17:24:01 +0600

    Version Update

--- config/tyro.php
+++ config/tyro.php
@@ -5,7 +5,7 @@ use HasinHayder\Tyro\Models\Role;
 use HasinHayder\Tyro\Models\UserRole;
 
 return [
-    'version' => env('TYRO_VERSION', '1.2.0'),
+    'version' => env('TYRO_VERSION', '1.2.3'),
 
     'disable_commands' => env('TYRO_DISABLE_COMMANDS', false),
 
