91735885f4244b2d93b3ab314daecdb9e5790e7d
Hasin Hayder <countdraculla@gmail.com>
2026-01-22 15:50:41 +0600

    Version Command Updated

--- src/Console/Commands/VersionCommand.php
+++ src/Console/Commands/VersionCommand.php
@@ -2,6 +2,8 @@
 
 namespace HasinHayder\Tyro\Console\Commands;
 
+use Illuminate\Console\Command;
+
 class VersionCommand extends BaseTyroCommand {
     protected $signature = 'tyro:version';
 
@@ -9,7 +11,21 @@ class VersionCommand extends BaseTyroCommand {
 
     public function handle(): int {
         $version = "1.2.3";
-        $this->info('Tyro v' . $version);
+        
+        $this->info('');
+        $this->info('  ╔════════════════════════════════════════╗');
+        $this->info('  ║                                        ║');
+        $this->info('  ║        Tyro                            ║');
+        $this->info('  ║                                        ║');
+        $this->info('  ╚════════════════════════════════════════╝');
+        $this->info('');
+        $this->info("  Version: <comment>{$version}</comment>");
+        $this->info('  Laravel: <comment>' . app()->version() . '</comment>');
+        $this->info('  PHP: <comment>' . PHP_VERSION . '</comment>');
+        $this->info('');
+        $this->info('  Documentation: <comment>https://hasinhayder.github.io/tyro/doc.html</comment>');
+        $this->info('  GitHub: <comment>https://github.com/hasinhayder/tyro</comment>');
+        $this->info('');
 
         return self::SUCCESS;
     }
