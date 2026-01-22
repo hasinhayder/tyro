a12e1e5081103c1f6283cbbca845f75673d2a89c
Hasin Hayder <countdraculla@gmail.com>
2026-01-22 15:36:02 +0600

    Tyro Version Fix

--- src/Console/Commands/VersionCommand.php
+++ src/Console/Commands/VersionCommand.php
@@ -8,7 +8,7 @@ class VersionCommand extends BaseTyroCommand {
     protected $description = 'Show the currently installed Tyro version';
 
     public function handle(): int {
-        $version = config('tyro.version', 'unknown');
+        $version = "1.2.3";
         $this->info('Tyro v' . $version);
 
         return self::SUCCESS;
